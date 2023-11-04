77596ca08b80a0bb9101cddc8924ccb99e804290
Tony Paulussen <tony.paulussen@wefabricate.com>
2023-11-04 22:14:59 +0100

    fix: create build scripts

---
+++ recipes/behaviortree-cpp/bld.bat
@@ -0,0 +1,20 @@
+mkdir build
+cd build
+
+cmake -G "NMake Makefiles" ^
+  %CMAKE_ARGS% ^
+  -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+  -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+  -DCMAKE_BUILD_TYPE=release ^
+  -DCMAKE_C_COMPILER=%CC% ^
+  -DCMAKE_CXX_COMPILER=%CXX% ^
+  ..
+  
+if errorlevel 1 exit /b 1
+
+cmake --build . --parallel -DPython3_EXECUTABLE="%PYTHON%"
+if errorlevel 1 exit /b 1
+cmake --install . -v
+if errorlevel 1 exit /b 1
+
+$env:PATH+=";Release"; tests/Release/behaviortree_cpp_test.exe
---
+++ recipes/behaviortree-cpp/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+mkdir -p build
+cd build
+cmake \
+  -G "Unix Makefiles" \
+  ${CMAKE_ARGS} \
+  -DCMAKE_PREFIX_PATH=${PREFIX} \
+  -DCMAKE_BUILD_TYPE=release \
+  -DCMAKE_C_COMPILER=${CC} \
+  -DCMAKE_CXX_COMPILER=${CXX} \
+  -DCMAKE_FIND_FRAMEWORK=NEVER \
+  -DCMAKE_FIND_APPBUNDLE=NEVER \
+  .. 
+  
+cmake --build . --parallel -DPython3_EXECUTABLE="$PYTHON"
+cmake --install . -v
+
+./tests/behaviortree_cpp_test
